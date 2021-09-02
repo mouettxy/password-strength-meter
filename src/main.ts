ea20690552eadfaa34de899e073e6358dabf00fa
Nick <lis@chaikovskie.com>
2021-09-02 19:31:37 +0300

    chore: prettify imports & update tsconfig & vue excplicit shims

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '~/App.vue'
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
-import './styles/main.css'
+import '~/styles/main.css'
 
 createApp(App).mount('#app')
